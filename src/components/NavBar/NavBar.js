eacd3327a2f2fb5193cf53319393e3903de08ee9
Calvin Huang <calhuang@ucdavis.edu>
2020-04-04 00:34:07 -0700

    added mobile responsiveness

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,45 +10,13 @@ class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      buttonList: [
-        {
-          name: 'Home',
-          char: 'ホーム',
-          img: null,
-          link: '/',
-        },
-        {
-          name: 'Experience',
-          char: '仕事の経験',
-          img: null,
-          link: '/exp',
-        },
-        {
-          name: 'Skills',
-          char: 'スキル',
-          img: null,
-          link: '/skills',
-        },
-        {
-          name: 'Resume',
-          char: '履歴書',
-          img: null,
-          link: '/resume',
-        },
-        {
-          name: 'Contact',
-          char: '個人情報',
-          img: null,
-          link: '/contact',
-        },
-      ],
       selectedButton: 0,
     };
   }
 
   componentDidMount() {
     // set current nav page
-    this.state.buttonList.forEach((nav, index) => {
+    this.props.buttonList.forEach((nav, index) => {
       if (this.props.location.pathname === nav.link) {
         this.setState({ selectedButton: index })
       }
@@ -65,7 +33,7 @@ class NavBar extends React.Component {
     return (
       <div className="menu">
         <div className="menu-container">
-          {this.state.buttonList.map((item, index) => 
+          {this.props.buttonList.map((item, index) => 
             <Link to={item.link} style={{ textDecoration: 'none' }} key={item.link}>
               <div className={index === this.state.selectedButton ? "menu-buttons selected" : "menu-buttons"} onClick={() => this.select(index)}>
                 <div className="jp-font jp-text">{item.char}</div>
